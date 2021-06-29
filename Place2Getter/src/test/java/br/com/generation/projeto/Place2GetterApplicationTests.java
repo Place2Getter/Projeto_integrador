7d9c077a7d17ed3af4fff60cd9c0500865fc410b
Lucas Cruz <lvplasey2@hotmail.com>
2021-06-29 11:21:42 -0300

    CheckPoint Tesk 3, subindo o projeto Spring

---
+++ Place2Getter/src/test/java/br/com/generation/projeto/Place2GetterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.generation.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Place2GetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
